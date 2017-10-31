d52e61de140e831068a5bca2ce85383937bb437a
Facundo Quiroga <facundoq@gmail.com>
2017-10-31 12:32:39 -0300

    added image class

--- util.py
+++ util.py
@@ -3,10 +3,10 @@ import matplotlib
 def video_rgb_to_hsv(video):
   T=video.shape[0]
   for t in range(T):
-    video[t,:,:,:]=matplotlib.colors.rgb_to_hsv(video[t,:]/255)
+    video[t,:,:,:]=matplotlib.colors.rgb_to_hsv(video[t,:])
 
 
 def video_hsv_to_rgb(video):
   T=video.shape[0]
   for t in range(T):
-    video[t,:,:,:]=matplotlib.colors.hsv_to_rgb(video[t,:])*255
+    video[t,:,:,:]=matplotlib.colors.hsv_to_rgb(video[t,:])
